4fc11b149e26b4a85d4e72bffbf61f5106a6288f
prin143s <princekgupta0000000000@gmail.com>
2026-08-09 20:12:46 +0530

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+ id("com.android.application")
+ id("org.jetbrains.kotlin.android")
+ id("org.jetbrains.kotlin.plugin.compose")
+}
+android {
+ namespace="com.chintucar.app"
+ compileSdk=35
+ defaultConfig {
+  applicationId="com.chintucar.app"
+  minSdk=24
+  targetSdk=35
+  versionCode=1
+  versionName="1.0"
+ }
+}
+dependencies {
+ implementation("androidx.core:core-ktx:1.15.0")
+ implementation("androidx.activity:activity-compose:1.10.1")
+ implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+ implementation("androidx.compose.ui:ui")
+ implementation("androidx.compose.ui:ui-tooling-preview")
+ implementation("androidx.compose.material3:material3")
+ implementation("androidx.compose.material:material-icons-extended")
+ implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
+}
