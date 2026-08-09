70dc5ba06d5cf0fc533e2509c86c0a1b607d6aa8
prin143s <princekgupta0000000000@gmail.com>
2026-08-09 20:49:16 +0530

    Refactor build.gradle.kts for Kotlin and Android setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,26 +1,38 @@
 plugins {
- id("com.android.application")
- id("org.jetbrains.kotlin.android")
- id("org.jetbrains.kotlin.plugin.compose")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
+
 android {
- namespace="com.chintucar.app"
- compileSdk=35
- defaultConfig {
-  applicationId="com.chintucar.app"
-  minSdk=24
-  targetSdk=35
-  versionCode=1
-  versionName="1.0"
- }
+    namespace = "com.chintucar.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.chintucar.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
+
+kotlin {
+    jvmToolchain(17)
+}
+
 dependencies {
- implementation("androidx.core:core-ktx:1.15.0")
- implementation("androidx.activity:activity-compose:1.10.1")
- implementation(platform("androidx.compose:compose-bom:2024.12.01"))
- implementation("androidx.compose.ui:ui")
- implementation("androidx.compose.ui:ui-tooling-preview")
- implementation("androidx.compose.material3:material3")
- implementation("androidx.compose.material:material-icons-extended")
- implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
 }
